f7a3b122fa27ec8d3adfbd74941a4b92c7d2d1dc
camilo <camilobotinagomez7@gmail.com>
2025-09-16 11:14:03 -0500

    mitienda

---
+++ resources/views/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+@section('title','Inicio')
+<h1>mi tienda </h1>
+
+<img src="https://jumbocolombiaio.vtexassets.com/arquivos/ids/205347-800-800?v=637814199798570000&width=800&height=800&aspect=true" class="img-fluid" alt="...">
+
+@section('content')
+
+@endsection
+
---
+++ routes/web.php
@@ -0,0 +1,5 @@
+<?php
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
+Route::view('/', 'index')->name('home');
+
